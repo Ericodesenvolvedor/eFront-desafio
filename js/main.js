d02875d742b409d49e5475c6861f41f8ac9a898b
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2023-02-12 13:36:20 -0300

    feat: Criando o desafio eFront.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const formSubmit = document.querySelector(".main__form-submit");
+const modal = document.querySelector(".modal");
+const modalWrapper = modal.querySelectorAll(".modal__wrapper");
+
+formSubmit.addEventListener("click", form);
+
+function form(event) {
+    event.preventDefault();
+    const inputName = document.querySelector("#name").value;
+    const inputEmail = document.querySelector("#email").value;
+
+    if(inputName && inputEmail) {
+        showModal(0, 'sucess');
+    } else {
+        showModal(1, 'error');
+    }
+}
+
+function showModal(indice, status) {
+    modal.classList.add("active");
+    modalWrapper[indice].classList.add(status);
+    
+    document.addEventListener("click", (event) => {
+        const {target} = event
+        
+        if(target.classList.contains("modal__close")) {
+            closeModal(indice, status);
+        }
+    })
+}
+
+function closeModal(indice, status) {
+    modalWrapper[indice].classList.remove(status);
+    modal.classList.remove("active");
+}
